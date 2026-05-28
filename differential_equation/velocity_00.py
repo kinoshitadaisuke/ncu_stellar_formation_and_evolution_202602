efd566d7635dadbf006d4a8b6498289724dce3b0
Daisuke <mhdf23888@gmail.com>
2026-05-28 15:58:20 +0800

    adding "differential_equation/velocity_00.py" on 28/May/2026

---
+++ differential_equation/velocity_00.py
@@ -0,0 +1,18 @@
+#!/usr/pkg/bin/python3.12
+
+#
+# Time-stamp: <2024/04/12 10:55:28 (UT+8) daisuke>
+#
+
+# importing numpy module
+import numpy
+
+# importing scipy module
+import scipy.constants
+
+# standard acceleration of gravity
+g = scipy.constants.physical_constants['standard acceleration of gravity']
+
+# printing result
+print (f'standard acceleration of gravity:')
+print (f' g = {g}')
