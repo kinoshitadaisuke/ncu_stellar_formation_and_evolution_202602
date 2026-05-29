7464b4fee732825cf89235901323f20d1d76bf24
Daisuke <mhdf23888@gmail.com>
2026-05-29 09:35:03 +0800

    adding "nuclear_reaction/nuclear_04.py" on 29/May/2026

--- nuclear_reaction/nuclear_04.py
+++ nuclear_reaction/nuclear_04.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.12
+#!/usr/bin/env python3
 
 #
-# Time-stamp: <2024/03/29 09:51:52 (UT+8) daisuke>
+# Time-stamp: <2026/05/28 16:10:12 (UT+08:00) daisuke>
 #
 
 # importing nuclyr module
