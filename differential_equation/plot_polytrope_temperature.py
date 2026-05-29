4b539219e6ef6fccd85d38ddd04505ca3de6c92b
Daisuke <mhdf23888@gmail.com>
2026-05-29 09:33:48 +0800

    adding "differential_equation/plot_polytrope_temperature.py" on 29/May/2026

--- differential_equation/plot_polytrope_temperature.py
+++ differential_equation/plot_polytrope_temperature.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.12
+#!/usr/bin/env python3
 
 #
-# Time-stamp: <2024/04/12 12:55:00 (UT+8) daisuke>
+# Time-stamp: <2026/05/28 16:06:59 (UT+08:00) daisuke>
 #
 
 # importing argparse module
