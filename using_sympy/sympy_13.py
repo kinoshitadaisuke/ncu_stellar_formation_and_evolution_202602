ea443ec9d8ddd66bc0ae3d21e687eddd326905fb
Daisuke <mhdf23888@gmail.com>
2026-05-29 09:36:18 +0800

    adding "using_sympy/sympy_13.py" on 29/May/2026

--- using_sympy/sympy_13.py
+++ using_sympy/sympy_13.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.12
+#!/usr/bin/env python3
 
 #
-# Time-stamp: <2024/05/09 17:01:22 (UT+8) daisuke>
+# Time-stamp: <2026/05/28 16:13:13 (UT+08:00) daisuke>
 #
 
 # importing sympy module
