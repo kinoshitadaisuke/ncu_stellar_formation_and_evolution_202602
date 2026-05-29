4ffd99d225969078f0bb2bab269db457eba46fb1
Daisuke <mhdf23888@gmail.com>
2026-05-29 09:34:29 +0800

    adding "differential_equation/trajectory_03.py" on 29/May/2026

--- differential_equation/trajectory_03.py
+++ differential_equation/trajectory_03.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.12
+#!/usr/bin/env python3
 
 #
-# Time-stamp: <2024/04/12 11:43:06 (UT+8) daisuke>
+# Time-stamp: <2026/05/28 16:08:37 (UT+08:00) daisuke>
 #
 
 # importing numpy module
